c3c272eb49a7f722bb6f386e25e88dd712c3d47d
JeonJunPyo02 <92019306+JeonJunPyo02@users.noreply.github.com>
2024-04-24 20:25:06 +0900

    ID 중복 처리

--- login.php
+++ login.php
@@ -1,19 +1,5 @@
 <?php
-// MySQL 데이터베이스 연결 정보
-$servername = "127.0.0.1"; // MySQL 서버 주소
-$username = "user"; // MySQL 계정 이름
-$password = "user1234"; // MySQL 계정 비밀번호
-$dbname = "account"; // 사용할 DB 이름
-
-// MySQL DB와 연결
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// 연결 확인
-if ($conn->connect_error) {
-    die("연결 실패: " . $conn->connect_error);
-} else {
-    // echo "Secceed!!!!";
-}
+include("dbconn.php");
 
 // input 태그에서 전달된 사용자 입력 값 가져오기
 $userid = $_POST['userid'];
--- signup.php
+++ signup.php
@@ -1,19 +1,6 @@
 <?php
-// MySQL 데이터베이스 연결 정보
-$servername = "127.0.0.1"; // MySQL 서버 주소
-$username = "user"; // MySQL 계정 이름
-$password = "user1234"; // MySQL 계정 비밀번호
-$dbname = "account"; // 사용할 DB 이름
-
-// MySQL DB와 연결
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// 연결 확인
-if ($conn->connect_error) {
-    die("연결 실패: " . $conn->connect_error);
-} else {
-    // echo "Secceed!!!!";
-}
+// DB 연결 정보 가져오기
+include("dbconn.php");
 
 // input 태그에서 전달된 사용자 입력 값 가져오기
 $userid = $_POST['userid'];
@@ -21,40 +8,43 @@ $userpw = $_POST['userpw'];
 $name = $_POST['name'];
 $email = $_POST['email'];
 
-// main 페이지
-$link_url = "index.php";
-$link_text = "메인 페이지로 돌아가기";
+// 중복된 id 체크 쿼리 작성
+$idcheck = $conn->prepare("SELECT userid FROM users WHERE userid=?");
+$idcheck->bind_param("s", $userid);
+$idcheck->execute();
 
-// SQL 쿼리 작성
-// userid=? -> 입력받을 파라미터
+// 반환된 SELECT 결과 저장
+$checkresult = $idcheck->get_result();
+
+// 회원 정보 DB 추가 쿼리 작성
 $sql = $conn->prepare("INSERT INTO users(userid,userpw,name,email,created) VALUES(?, ?, ?, ?, NOW())");
-// 입력받을 파라미터 -> 문자열이면 "s" 정수면 "i"
 $sql->bind_param("ssss", $userid, $userpw, $name, $email);
 
-// 쿼리 실행
-$sql->execute();
-
-// 영향 받은 행 개수 확인
-$result = $sql->affected_rows;
-
-// 결과 확인
-if ($result > 0) {
-    
-    // 회원가입 성공
-    echo "회원가입 성공!";
-
-    // index.php 페이지로 돌아가기
-    echo "<p><a href='$link_url'>$link_text</a></p>";
-
+// 반환된 SELECT 결과의 개수 확인(num_rows)
+if ($checkresult->num_rows > 0) {
+    echo "<script>alert('중복된 ID 입니다!!');</script>";
+?>
+    <script>
+        location.replace('signup.html')
+    </script>
+<?php
 } else {
-    // 회원가입 실패
-    echo "회원가입 실패..";
-
-    // index.php 페이지로 돌아가기
-    echo "<p><a href='$link_url'>$link_text</a></p>";
+    $sql->execute();
+    // 쿼리로 영향을 받은 행 개수 확인(affected_rows)
+    $result = $sql->affected_rows;
+    if ($result > 0) {
+        echo "<script>alert('회원가입 성공!!');</script>";
+    } else {
+        echo "<script>alert('회원가입 실패..');</script>";
+    }
 }
 
 // 연결 닫기
 $sql->close();
+$idcheck->close();
 $conn->close();
 ?>
+
+<script>
+    location.replace('index.php')
+</script>
